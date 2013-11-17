e0385a8da1a3d8443d274286b24fa9e14d107414
Tijl Van Assche <tijlvanassche@gmail.com>
2013-11-17 14:00:06 +0100

    add license

--- fairshare.sh
+++ fairshare.sh
@@ -1,2 +1,18 @@
 #! /bin/bash
+# Fairshare - A small command line application to manage shared expenses.
+# Copyright (C) 2013  Tijl Van Assche <tijlvanassche@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 python3 fairshare.py "$@"
